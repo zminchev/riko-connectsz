a9e67ba6c77531e8eb2507b5905617747c8af82d
Zhivko Minchev <zhivko.minchev@mentormate.com>
2024-09-24 18:01:47 +0300

    More redirect logic

--- src/pages/chats/[...slug].tsx
+++ src/pages/chats/[...slug].tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  PreviewData,
+} from "next";
+import { ParsedUrlQuery } from "querystring";
+import React from "react";
+import { createClient } from "src/utils/supabase/server-props";
 
 const ChatsPage = () => {
-  return (
-    <div>ChatsPage</div>
-  )
-}
+  return <div>ChatsPage</div>;
+};
 
-export default ChatsPage
+export default ChatsPage;
+
+export const getServerSideProps = async (
+  ctx: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
+) => {
+  //@ts-ignore
+  const supabase = createClient({ req: ctx.req, res: ctx.res });
+  const { data: userData } = await supabase.auth.getUser();
+  if (!userData.user) {
+    return {
+      redirect: {
+        destination: `/login`,
+        permanent: false,
+      },
+    };
+  }
+
+  const { data: chat, error } = await supabase
+    .from("chats")
+    .select("*")
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return {
+    props: {
+      chat,
+    },
+  };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  PreviewData,
+} from "next";
+import { ParsedUrlQuery } from "querystring";
+import { createClient } from "src/utils/supabase/server-props";
+
 export default function Home() {
   return <div>test</div>;
 }
+
+export const getServerSideProps = async (
+  ctx: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
+) => {
+  //@ts-ignore
+  const supabase = createClient({ req: ctx.req, res: ctx.res });
+
+  const { data } = await supabase.auth.getUser();
+  const { data: chat, error } = await supabase
+    .from("chats")
+    .select("*")
+    .single();
+
+  if (data.user) {
+    return {
+      redirect: {
+        destination: `/chats/${chat.id}`,
+        permanent: false,
+      },
+    };
+  }
+};
