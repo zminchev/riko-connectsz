0c20e5035ffe194f0d62b5d2824a40662e69801d
Zhivko Minchev <jivkominchev.j@gmail.com>
2024-09-27 00:38:31 +0300

    Test

--- src/utils/supabase/component.ts
+++ src/utils/supabase/component.ts
@@ -1,5 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+console.log('asd')
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL || "",
   process.env.NEXT_PUBLIC_SUPABASE_KEY || ""
